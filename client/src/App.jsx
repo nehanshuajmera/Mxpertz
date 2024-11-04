34f695f5477e015e89a16cdd6f5c37aeaca543bc
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-11-05 00:17:02 +0530

    Initializing App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,35 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import { useState, useEffect } from 'react';
-import axois from 'axios';
-import { Header } from "./components/header/header.component";
-import { HomePage } from "./pages/homepage/homepage.component";
-import { AdventurePage } from "./pages/adventure-page/adventure-page.component";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await axois.get(
-        `https://mxpertztestapi.onrender.com/api/adventure`
-      );
-      setData(response.data);
-    } catch (err) {
-      console.error({ error: err.message });
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage data={data} />} />
-        <Route path="/adventure/:id" element={<AdventurePage data={data}/>} />
-      </Routes>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
