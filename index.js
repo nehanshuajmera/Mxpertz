2e604757f44285c9ce63712e57ab305f387cc723
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-11-07 04:37:30 +0530

    Added CORS Configuration

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ import { userRoutes } from "./routes/userRoutes.js";
 /* creating express app */
 const app = express();
 
+/* CORS Configuration */
+const corsOptions = {
+  origin: ["http://localhost:5173",],
+  credentials: true,
+};
+
 /* middleware */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 /* routes */
